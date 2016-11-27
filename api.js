7fc5c7a4b417ad39be642216e588694da9d23837
Tyrannas <someonefefe@gmail.com>
2016-11-27 07:34:48 +0100

    api

---
+++ api.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoRequest = require("./request/request");
+const PORT = 8080;
+
+
+app = new express();
+app.use(bodyParser.json());
+
+mongoRequest.connect();
+
+app.get('/user/:id', function(req,res,err){
+	var id = req.params.id;
+	var obj = mongoRequest.getUser(id, data => {console.log(data); res.json(data)});
+});
+
+app.put('/user/:id', function(req, res, err){
+	var id = req.params.id;
+	console.log('on a une requete node');
+});
+
+app.post('/user/', function(req, res, err){
+	var json = req.body;
+	console.log(json);
+	mongoRequest.addUser(json);
+});
+
+app.post('/users/', function(req, res, err){
+	var json = req.body;
+	for(e of json){
+		//console.log(e);
+		mongoRequest.addUser(e);
+	}
+});
+
+app.get('/room/:id', function(req, res, err){
+	var id = req.params.id;
+	var obj = mongoRequest.getRoom(id, data => {console.log(data); res.json(data)});
+});
+
+app.listen(PORT, function(){
+	console.log("WAW ON EST CONNECTES A UN SUPER SERVEUR. WAW AS TU VU? WAW. RENVERSANT. SPECTACULAAAAAAIRE.");
+});
