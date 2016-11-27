43eb0410ac3292efe7673fae2051b52ea19cd4d1
Tyrannas <someonefefe@gmail.com>
2016-11-27 07:53:26 +0100

    cors

--- api.js
+++ api.js
@@ -6,6 +6,11 @@ const PORT = 8080;
 
 app = new express();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoRequest.connect();
 
